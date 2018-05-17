582c71ee39e2ef4ab8fb94d7950d68ba6c15f80c
Emirhan <emirhantje75@gmail.com>
2018-05-17 12:41:45 +0200

    Add project files.

---
+++ GameShopDemirci/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Write your JavaScript code.
+$("#pc").click(function () {
+    alert("pc is clicked");
+});
+$("#ps4").click(function () {
+    alert("ps 4 is clicked");
+});
+$("#xbox").click(function () {
+    alert("XBOX is clicked");
+});
+$("#nintendo").click(function () {
+    alert("Nintendo is clicked");
+});
